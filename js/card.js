64b05672c6b9cc8bcab00612a7c67c5c3ed5a3d8
ehh99 <jinung5@gmail.com>
2024-04-30 14:49:53 +0900

    scripting 요소

--- js/card.js
+++ js/card.js
@@ -1,6 +1,11 @@
-const button = document.querySelector('.flip-button');
-const card = document.querySelector('.card');
+let count = 0;
+const button = document.querySelector(".flip-button");
+const card = document.querySelector(".card");
 
-button.addEventListener('click', () => {
-  card.classList.toggle('is-flipped');
+button.addEventListener("click", () => {
+  count++;
+  card.classList.toggle("is-flipped");
+  count % 2 === 0
+    ? (button.textContent = "카드 뒤집기")
+    : (button.textContent = "카드 뒤집기 완료");
 });
